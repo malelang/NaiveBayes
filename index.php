c417c2704931f1d3505259d93c7b4072d1bc76e1
malelang <landiezm@unicauca.edu.co>
2018-03-11 09:09:05 -0500

    git add .

---
+++ index.php
@@ -0,0 +1,40 @@
+<html>
+   <head>
+      <title>The Materialize Form Example</title>
+      <meta name = "viewport" content = "width = device-width, initial-scale = 1">
+      <link rel = "stylesheet"
+         href = "https://fonts.googleapis.com/icon?family=Material+Icons">
+      <link rel = "stylesheet"
+         href = "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/css/materialize.min.css">
+      <script type = "text/javascript"
+         src = "https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script src = "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js">
+      </script>
+   </head>
+
+   <body class = "container">
+        <nav>
+          <div class="nav-wrapper">
+            <a href="#" class="brand-logo center">Mineria de Datos</a>
+
+          </div>
+        </nav>
+        <nav>
+          <div class="nav-wrapper">
+            <ul id="nav-mobile" class="left hide-on-med-and-down">
+              <li><a href="index.php">Inicio</a></li>
+              <li><a href="send.php">Guardar Registros</a></li>
+              <li><a href="list.php">Listar Registros</a></li>
+              <li><a href= "cross.php">Validacion cruzada</a><li>
+            </ul>
+          </div>
+        </nav>
+        <br>
+        <br>
+
+        <h3>Inicio</h3>
+        <br>
+        <img src="http://synergymaxlearn.com/wp-content/uploads/2016/07/data-mining.png" alt=""/>
+
+       </body>
+</html>
